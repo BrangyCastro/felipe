c632bdede7215610215f4e4fc725efd07eb76202
Brangy Castro <brangy.castro1@gmail.com>
2022-01-27 22:30:31 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'felipe';
+
+  data = [
+    {
+      id: '1',
+      nombre_pasaje: 'Nombre 1',
+      departamento: 'depa 1',
+      municipio: 'Municipio 1',
+      estado_solicitud: 'activo',
+      categoria: 'n/d',
+      usuario: 'n/d',
+    },
+    {
+      id: '2',
+      nombre_pasaje: 'Nombre 1',
+      departamento: 'depa 1',
+      municipio: 'Municipio 1',
+      estado_solicitud: 'activo',
+      categoria: 'n/d',
+      usuario: 'n/d',
+    },
+    {
+      id: '3',
+      nombre_pasaje: 'Nombre 1',
+      departamento: 'depa 1',
+      municipio: 'Municipio 1',
+      estado_solicitud: 'activo',
+      categoria: 'n/d',
+      usuario: 'n/d',
+    },
+    {
+      id: '4',
+      nombre_pasaje: 'Nombre 1',
+      departamento: 'depa 1',
+      municipio: 'Municipio 1',
+      estado_solicitud: 'activo',
+      categoria: 'n/d',
+      usuario: 'n/d',
+    },
+  ]
+
+  onClickNuevoPasaje() {
+    console.log('Agregar un nuevo pasaje');
+  }
+
+  onClickVer() {
+    console.log('ver los pasajes');
+  }
+
+  onClickEditar() {
+    console.log('Editar los pasajeros');
+  }
+
+  onClickEliminar() {
+    console.log('Eliminar pasajeros');
+  }
 }
